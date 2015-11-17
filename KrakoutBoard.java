3e0be3739d776bc9a437de2a4da4b86ab8778285
XuanyiZhu <xzhu2@uvm.edu>
2015-11-17 00:44:03 -0500

    Update KrakoutBoard.java

--- KrakoutBoard.java
+++ KrakoutBoard.java
@@ -4,34 +4,31 @@
  * and open the template in the editor.
  */
 
-
-
 import javafx.scene.layout.Pane;
-
 
 /**
  * A Pane in which tetris squares can be displayed.
- * 
+ *
  * @author Xuanyi Zhu
  */
-public class KrakoutBoard extends Pane{
-    // The size of the side of a tetris square
+public class KrakoutBoard extends Pane {
+
+    // The size of the side of a krakout square
+
     public static final int SQUARE_SIZE = 20;
     // The number of squares that fit on the screen in the x and y dimensions
     public static final int X_DIM_SQUARES = 20;
     public static final int Y_DIM_SQUARES = 30;
-    public static final double BoardLength = 0.15;
-
+    public static final double BoardLength = 0.2;
 
     /**
      * Sizes the board to hold the specified number of squares in the x and y
      * dimensions.
      */
     public KrakoutBoard() {
-        this.setPrefHeight(Y_DIM_SQUARES*SQUARE_SIZE);
-        this.setPrefWidth(X_DIM_SQUARES*SQUARE_SIZE);
+        this.setPrefHeight(Y_DIM_SQUARES * SQUARE_SIZE);
+        this.setPrefWidth(X_DIM_SQUARES * SQUARE_SIZE);
 
     }
 
-    
 }
