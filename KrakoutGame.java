a5caa7ccda0e47fa2f77661eb26cfb6ca6c518b4
XuanyiZhu <xzhu2@uvm.edu>
2015-11-17 00:44:19 -0500

    Update KrakoutGame.java

--- KrakoutGame.java
+++ KrakoutGame.java
@@ -4,23 +4,25 @@
  * and open the template in the editor.
  */
 
-
-
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Color;
-
 
 /**
  * This should be implemented to include your game control.
+ *
  * @author pipWolfe
  */
 public class KrakoutGame {
+
     private final Krakout krakoutApp;
     private final DrawingBoard line;
     static boolean BallIsMove;
     private KrakoutBall ball;
+
     /**
-     * Initialize the game. Remove the example code and replace with code
-     * that creates a random piece.
+     * Initialize the game. Remove the example code and replace with code that
+     * creates a random piece.
+     *
      * @param krakoutApp
      * @param board A reference to the board on which Squares are drawn
      */
@@ -34,44 +36,38 @@ public class KrakoutGame {
 //        KrakoutSquare square2 = new KrakoutSquare(board);
 //        square2.moveToKrakoutLocation(5, 3);
 //        square2.setColor(Color.RED);
-        
-        DrawingBoard line = new DrawingBoard(board);
-        line.setLine(Color.BLACK,5.0);
-        this.line = line;
-        
 
-//        helper.setBall(Color.BLACK, Color.WHITE);
-        
+        DrawingBoard line = new DrawingBoard(board);
+        line.setLine(Color.BLACK, 5.0);
+        this.line = line;
+
         KrakoutBall ball = new KrakoutBall(board, line);
         ball.setBall(Color.BLACK, Color.WHITE);
         this.ball = ball;
-        
+
         this.krakoutApp = krakoutApp;
         // You can use this to show the score, etc.
         krakoutApp.setMessage("Game has started!");
     }
 
     /**
-     * Animate the game, by moving the current tetris piece down.
+     * Animate the game, by moving the current board left or right.
      */
     void update() {
-  
-        
-        System.out.println("before  ball  X=" + ball.getCenterX()+"    Y="+ ball.getCenterY());
-       ball.moveBall(ball.updateVector().getX()+ball.getCenterX(),ball.updateVector().getY()+ball.getCenterY());
-        System.out.println("after  ball  X=" + ball.getCenterX()+"    Y="+ ball.getCenterY());
+        Point2D directionVector = ball.updateVector();
+        ball.moveBall(directionVector.getX() + ball.getCenterX(), directionVector.getY() + ball.getCenterY());
     }
-    
+
     /**
-     * Move the current tetris piece left.
+     * Move the current krakout piece left.
      */
     void left() {
-        
+
         System.out.println("left key was pressed!");
-        if (line.getStartX()>7){
-            line.moreBoard(line.getStartX()-7,line.getEndX()-7);
+        if (line.getStartX() > 10) {
+            line.moreBoard(line.getStartX() - 10, line.getEndX() - 10);
         }
-        
+
     }
 
     /**
@@ -79,14 +75,10 @@ public class KrakoutGame {
      */
     void right() {
         System.out.println("right key was pressed!");
-        if (line.getEndX()<(KrakoutBoard.X_DIM_SQUARES*KrakoutBoard.SQUARE_SIZE-7)){
-            line.moreBoard(line.getStartX()+7,line.getEndX()+7);
+        if (line.getEndX() < (KrakoutBoard.X_DIM_SQUARES * KrakoutBoard.SQUARE_SIZE - 10)) {
+            line.moreBoard(line.getStartX() + 10, line.getEndX() + 10);
         }
-        
+
     }
-    
-    void space(){
-        
-        
-    }
+
 }
